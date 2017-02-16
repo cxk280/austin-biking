e62b646fccd6b793f9ec8546f20b8712631a1a6b
Christopher King <chrisking1@gmail.com>
2017-02-16 12:36:08 -0500

    basic google maps init components created, still need to troubleshoot errors

--- src/App.js
+++ src/App.js
@@ -39,14 +39,57 @@ class AddressField extends Component {
 //   }
 // }
 
-// class GoogleMaps extends Component {
-//   render() {
-//     return (
-//       <div>
-//       </div>
-//     )
-//   }
-// }
+class GoogleMapsInitScript extends Component {
+  render() {
+    return (
+      <div>
+        const initScript = document.createElement("script");
+
+        initScript.innerHTML = "let map;
+            function initMap() {
+              map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 30.3074624, lng: -98.0336022},
+                zoom: 8
+              })
+            }"
+
+        document.body.appendChild(script);
+      </div>
+    )
+  }
+}
+
+class GoogleMapsSrcScript extends Component {
+  render() {
+    return (
+      <div>
+        const SrcScript = document.createElement("script");
+
+        script.src = "https://maps.googleapis.com/maps/api/js?key=" + process.env.REACT_APP_GOOGLE_MAPS_API_KEY + "&callback=initMap";
+        script.async = true;
+        script.defer = true;
+
+        document.body.appendChild(script);
+      </div>
+    )
+  }
+}
+
+class GoogleMaps extends Component {
+  render() {
+    return (
+      <div id="map">
+        <GoogleMapsInitScript />
+        <GoogleMapsSrcScript />
+      </div>
+    )
+  }
+}
+
+//Add the following to a CSS sheet eventually:
+// #map {
+//         height: 100%;
+//       }
 
 
 
@@ -61,6 +104,9 @@ class App extends Component {
           Austin, Texas has many wonderful urban trails perfect for biking. Enter your current location below and we'll find the trail nearest you.
         </p>
         <AddressField formAction="#" formLabel="Address: " />
+        <br />
+        <br />
+        <GoogleMaps />
       </div>
     );
   }
